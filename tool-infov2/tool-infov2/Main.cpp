fb6808eb8be3a52e82ddc460e8831afcf0bb6146
Samu <csonka.samuel@gmail.com>
2023-10-20 19:34:37 +0200

    initialization, base, main funciton

---
+++ tool-infov2/tool-infov2/Main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int hwidInfo()
+{
+	return 0;
+}
+
+int cpuInfo()
+{
+	return 0;
+}
+
+int main()
+{
+	int key;
+	system("cls");
+	cout << "[1] HWID" << endl; cout << "[2] CPU infos" << endl; cout << "[3] exit" << endl;
+	cout << "key: ";
+	cin >> key;
+	cout << key;
+	switch (key)
+	{
+		case 1:
+			hwidInfo();
+		case 2:
+			cpuInfo();
+		case 3:
+			exit(0);
+		default:
+			system("cls");
+			cout << "Please enter a vaild number!" << endl;
+			system("timeout 2");
+	}
+}
